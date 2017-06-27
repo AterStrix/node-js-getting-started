5d30cdaeb47829b94fab40332404a6df9af17235
AterStrix <artem.aterstrix@gmail.com>
2017-06-27 23:18:45 +0300

    Added json server.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
-
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const http = require('http');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -13,12 +16,44 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/db', function(request, response) {
-  response.render('pages/db');
+app.get('/api/**', function(request, response) {
+	var options = {
+		host: request.hostname,
+		port: 3000,
+		path: request.url,
+		method: request.method,
+		params: request.params
+	};
+	var req = http.request(options, function(res) {
+		res.setEncoding('utf8');
+
+		res.on('data', function (chunk) {
+			if (res.statusCode >= 400) {
+				response
+					.status(res.statusCode)
+					.send({
+						type: "error", 
+						statusCode: res.statusCode,
+						statusMessage: res.statusMessage
+					});
+			}
+			else {
+				response.write(chunk);
+			}
+		});
+		res.on('end', function(){
+			response.end();
+		});
+	});
+	req.end();
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+server.use("/api", router);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
 
