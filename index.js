793152fb8fea78e60fcc79bb4f32eaa823874560
AterStrix <artem.aterstrix@gmail.com>
2017-06-30 23:25:36 +0300

    Added cors

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/api/**', function(request, response) {
 });
 
 server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'course-todo-list.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Access-Control-Allow-Origin');
   next();
