2eeb11ceecc7f737f815a34494d9bdcbd8bc2c96
AterStrix <artem.aterstrix@gmail.com>
2017-07-03 09:12:32 +0300

    Configured API.

--- index.js
+++ index.js
@@ -1,52 +1,6 @@
-var express = require('express');
-var app = express();
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const http = require('http');
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/api/**', function(request, response) {
-	var options = {
-		host: request.hostname,
-		port: 3000,
-		path: request.url,
-		method: request.method,
-		params: request.params
-	};
-	var req = http.request(options, function(res) {
-		res.setEncoding('utf8');
-
-		res.on('data', function (chunk) {
-			if (res.statusCode >= 400) {
-				response
-					.status(res.statusCode)
-					.send({
-						type: "error", 
-						statusCode: res.statusCode,
-						statusMessage: res.statusMessage
-					});
-			}
-			else {
-				response.write(chunk);
-			}
-		});
-		res.on('end', function(){
-			response.end();
-		});
-	});
-	req.end();
-});
 
 server.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://course-todo-list.herokuapp.com');
@@ -55,14 +9,8 @@ server.use((req, res, next) => {
   next();
 });
 
-/*router.render = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-}*/
-
-server.use(router);
-server.listen(app.get('port'), () => {
+server.use('/api', router);
+server.listen((process.env.PORT != 5000 ? process.env.PORT : 3000), () => {
   console.log('JSON Server is running');
 });
 
